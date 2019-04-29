52d07d0add328668c011edb1949fcc8771234e11
Kelly McLaughlin <kelly.mclaughlin@joyent.com>
2019-04-29 14:34:37 -0600

    Convert electric-moray service setup files for electric-boray

--- boot/setup.sh
+++ boot/setup.sh
@@ -6,11 +6,11 @@
 #
 
 #
-# Copyright (c) 2018, Joyent, Inc.
+# Copyright (c) 2019, Joyent, Inc.
 #
 
 #
-# Bootstraps the consistent hash ring for electric-moray.
+# Bootstraps the consistent hash ring for electric-boray.
 #
 
 set -o xtrace
@@ -23,7 +23,7 @@ if [[ -h $SOURCE ]]; then
 fi
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 PROFILE=/root/.bashrc
-SVC_ROOT=/opt/smartdc/electric-moray
+SVC_ROOT=/opt/smartdc/electric-boray
 
 source ${DIR}/scripts/util.sh
 source ${DIR}/scripts/services.sh
@@ -31,8 +31,8 @@ source ${DIR}/scripts/services.sh
 export PATH=$SVC_ROOT/bin:$SVC_ROOT/build/node/bin:/opt/local/bin:/usr/sbin/:/usr/bin:$PATH
 export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 
-FASH=/opt/smartdc/electric-moray/node_modules/.bin/fash
-LEVELDB_DIR_PARENT=/electric-moray/chash
+FASH=/opt/smartdc/electric-boray/node_modules/.bin/fash
+LEVELDB_DIR_PARENT=/electric-boray/chash
 LEVELDB_DIR=$LEVELDB_DIR_PARENT/leveldb-
 SAPI_URL=$(mdata-get SAPI_URL)
 [[ -n $SAPI_URL ]] || fatal "no SAPI_URL found"
@@ -74,7 +74,7 @@ export SDC_IMGADM_URL=$(json metadata.HASH_RING_IMGAPI_SERVICE <<< "$manta_app")
 [[ -n $SDC_IMGADM_URL ]] || fatal "no SDC_IMGADM_URL found"
 ZONE_UUID=$(/usr/bin/zonename)
 ZFS_PARENT_DATASET=zones/$ZONE_UUID/data
-ZFS_DATASET=$ZFS_PARENT_DATASET/electric-moray
+ZFS_DATASET=$ZFS_PARENT_DATASET/electric-boray
 
 function manta_hack_syslog_conf {
     # Hack.  See MANTA-2165
@@ -86,17 +86,17 @@ function manta_hack_syslog_conf {
 }
 
 function manta_setup_determine_instances {
-    ELECTRIC_MORAY_INSTANCES=1
+    ELECTRIC_BORAY_INSTANCES=1
     local size=`json -f ${METADATA} SIZE`
     if [ "$size" = "lab" ] || [ "$size" = "production" ]
     then
-        ELECTRIC_MORAY_INSTANCES=4
+        ELECTRIC_BORAY_INSTANCES=4
     fi
 }
 
 function manta_setup_leveldb_hash_ring {
     # get the hash ring image
-    /opt/smartdc/electric-moray/node_modules/.bin/sdc-imgadm get-file $HASH_RING_IMAGE -o $HASH_RING_FILE
+    /opt/smartdc/electric-boray/node_modules/.bin/sdc-imgadm get-file $HASH_RING_IMAGE -o $HASH_RING_FILE
     local leveldb_ring_parent_dir=/var/tmp/$(uuid -v4)
     local leveldb_ring=$leveldb_ring_parent_dir/hash_ring
     mkdir -p $leveldb_ring_parent_dir
@@ -119,7 +119,7 @@ function manta_setup_leveldb_hash_ring {
     [[ $? -eq 0 ]] || fatal "unable to setup leveldb"
     # build the list of leveldb locations
     local leveldb_dirs
-    for (( i=1; i<=$ELECTRIC_MORAY_INSTANCES; i++ ))
+    for (( i=1; i<=$ELECTRIC_BORAY_INSTANCES; i++ ))
     do
         leveldb_dirs[$i]=$LEVELDB_DIR$(expr 2020 + $i)
     done
@@ -139,12 +139,12 @@ function manta_setup_leveldb_hash_ring {
     [[ $? -eq 0 ]] || fatal "unable to setup leveldb"
 }
 
-function manta_setup_electric_moray {
+function manta_setup_electric_boray {
     #Build the list of ports.  That'll be used for everything else.
     local ports
     local kangs
     local statuses
-    for (( i=1; i<=$ELECTRIC_MORAY_INSTANCES; i++ )); do
+    for (( i=1; i<=$ELECTRIC_BORAY_INSTANCES; i++ )); do
         ports[$i]=`expr 2020 + $i`
         kangs[$i]=`expr 3020 + $i`
         statuses[$i]=`expr 4020 + $i`
@@ -170,10 +170,10 @@ function manta_setup_electric_moray {
 
     #haproxy
     for port in "${ports[@]}"; do
-        hainstances="$hainstances        server electric-moray-$port 127.0.0.1:$port check inter 10s slowstart 10s error-limit 3 on-error mark-down\n"
+        hainstances="$hainstances        server electric-boray-$port 127.0.0.1:$port check inter 10s slowstart 10s error-limit 3 on-error mark-down\n"
     done
 
-    sed -e "s#@@ELECTRIC-MORAY_INSTANCES@@#$hainstances#g" \
+    sed -e "s#@@ELECTRIC-BORAY_INSTANCES@@#$hainstances#g" \
         $SVC_ROOT/etc/haproxy.cfg.in > $SVC_ROOT/etc/haproxy.cfg || \
         fatal "could not process $src to $dest"
 
@@ -181,33 +181,33 @@ function manta_setup_electric_moray {
         fatal "unable to import haproxy"
     svcadm enable "manta/haproxy" || fatal "unable to start haproxy"
 
-    #electric-moray instances
-    local electric_moray_xml_in=$SVC_ROOT/smf/manifests/electric-moray.xml.in
-    for (( i=1; i<=$ELECTRIC_MORAY_INSTANCES; i++ )); do
+    #electric-boray instances
+    local electric_boray_xml_in=$SVC_ROOT/smf/manifests/electric-boray.xml.in
+    for (( i=1; i<=$ELECTRIC_BORAY_INSTANCES; i++ )); do
         local port=${ports[$i]}
         local kang=${kangs[$i]}
         local status=${statuses[$i]}
-        local electric_moray_instance="electric-moray-$port"
-        local electric_moray_xml_out=$SVC_ROOT/smf/manifests/electric-moray-$port.xml
-        sed -e "s#@@ELECTRIC-MORAY_PORT@@#$port#g" \
+        local electric_boray_instance="electric-boray-$port"
+        local electric_boray_xml_out=$SVC_ROOT/smf/manifests/electric-boray-$port.xml
+        sed -e "s#@@ELECTRIC-BORAY_PORT@@#$port#g" \
             -e "s#@@KANG_PORT@@#$kang#g" \
             -e "s#@@STATUS_PORT@@#$status#g" \
-            -e "s#@@ELECTRIC-MORAY_INSTANCE_NAME@@#$electric_moray_instance#g" \
-            $electric_moray_xml_in  > $electric_moray_xml_out || \
-            fatal "could not process $electric_moray_xml_in to $electric_moray_xml_out"
+            -e "s#@@ELECTRIC-BORAY_INSTANCE_NAME@@#$electric_boray_instance#g" \
+            $electric_boray_xml_in  > $electric_boray_xml_out || \
+            fatal "could not process $electric_boray_xml_in to $electric_boray_xml_out"
 
-        svccfg import $electric_moray_xml_out || \
-            fatal "unable to import $electric_moray_instance: $electric_moray_xml_out"
-        svcadm enable "$electric_moray_instance" || \
-            fatal "unable to start $electric_moray_instance"
+        svccfg import $electric_boray_xml_out || \
+            fatal "unable to import $electric_boray_instance: $electric_boray_xml_out"
+        svcadm enable "$electric_boray_instance" || \
+            fatal "unable to start $electric_boray_instance"
     done
 
     unset IFS
 }
 
-function manta_setup_moray_rsyslogd {
+function manta_setup_boray_rsyslogd {
     #rsyslog was already set up by common setup- this will overwrite the
-    # config and restart since we want moray to log locally.
+    # config and restart since we want boray to log locally.
     local domain_name=$(json -f ${METADATA} domain_name)
     [[ $? -eq 0 ]] || fatal "Unable to domain name from metadata"
 
@@ -250,7 +250,7 @@ HERE
 # Support node bunyan logs going to local0 and forwarding
 # only as logs are already captured via SMF
 # Uncomment the following line to get local logs via syslog
-local0.* /var/log/electric-moray.log;bunyan
+local0.* /var/log/electric-boray.log;bunyan
 local0.* @@ops.$domain_name:10514
 
 HERE
@@ -265,7 +265,7 @@ HERE
     [[ $? -eq 0 ]] || fatal "Unable to restart rsyslog"
 
     #log pulling
-    manta_add_logadm_entry "electric-moray" "/var/log" "exact"
+    manta_add_logadm_entry "electric-boray" "/var/log" "exact"
 }
 
 # Mainline
@@ -277,20 +277,19 @@ echo "Running common setup scripts"
 manta_common_presetup
 
 echo "Adding local manifest directories"
-manta_add_manifest_dir "/opt/smartdc/electric-moray"
+manta_add_manifest_dir "/opt/smartdc/electric-boray"
 
-manta_common_setup "electric-moray" 0
+manta_common_setup "electric-boray" 0
 
 manta_setup_determine_instances
 
-echo "Setting up leveldb"
-manta_setup_leveldb_hash_ring
+# echo "Setting up leveldb"
+# manta_setup_leveldb_hash_ring
 
-echo "Setting up e-moray"
-manta_setup_electric_moray
-manta_setup_moray_rsyslogd
+echo "Setting up e-boray"
+manta_setup_electric_boray
+manta_setup_boray_rsyslogd
 
 manta_common_setup_end
 
 exit 0
-
